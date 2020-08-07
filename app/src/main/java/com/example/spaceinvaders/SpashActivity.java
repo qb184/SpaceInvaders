ff4d41c69090d6c9b25d7a0fc457439a1a934166
qb184 <quynhbui184@gmail.com>
2020-08-07 14:32:00 -0400

    add help button in SplashActivity, pause and resume buttons in MainActivity

--- app/src/main/java/com/example/spaceinvaders/SpashActivity.java
+++ app/src/main/java/com/example/spaceinvaders/SpashActivity.java
@@ -2,13 +2,30 @@ package com.example.spaceinvaders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SpashActivity extends AppCompatActivity {
-
+    private Button playBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spash);
+        setContentView(R.layout.activity_splash);
+        playBtn = findViewById(R.id.playBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+
     }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
